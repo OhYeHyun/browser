9496a40b64c65c6a699745deb168234e0246dcf9
OhYeHyun <107532928+OhYeHyun@users.noreply.github.com>
2023-11-17 19:25:46 +0900

    Add files via upload

--- chap 1/coordinates/main.js
+++ chap 1/coordinates/main.js
@@ -2,19 +2,21 @@ const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 const horizontal = document.querySelector(".horizontal");
 const vertical = document.querySelector(".vertical");
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
 document.addEventListener("mousemove", (e) => {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
 
-  target.style.left = mouseX + "px";
-  target.style.top = mouseY + "px";
+  vertical.style.transform = `translateX(${mouseX}px)`;
+  horizontal.style.transform = `translateY(${mouseY}px)`;
+  target.style.transform = `translate(${mouseX - targetHalfWidth}px, ${
+    mouseY - targetHalfHeight
+  }px)`;
 
-  vertical.style.left = mouseX + "px";
-  horizontal.style.top = mouseY + "px";
-
-  tag.style.left = mouseX + 20 + "px";
-  tag.style.top = mouseY + 10 + "px";
+  tag.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
 
   tag.innerHTML = `
   ${mouseX}px, ${mouseY}px`;
